876c8dcaf01a1c83340e37dd671e71d193cba2bc
krey35 <kenneth.reyes@kellogg.northwestern.edu>
2022-06-06 02:19:07 +0000

    initial save - migrations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.all 
+  end 
+  
   def new
     @post = Post.new
     @post.place_id = params["place_id"]
@@ -9,8 +13,10 @@ class PostsController < ApplicationController
     @post = Post.new
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
+    @post.uploaded_image.attach(params["post"]["uploaded_image"])
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
+    @post["user_id"] = @current_user["id"]
     @post.save
     redirect_to "/places/#{@post["place_id"]}"
   end
